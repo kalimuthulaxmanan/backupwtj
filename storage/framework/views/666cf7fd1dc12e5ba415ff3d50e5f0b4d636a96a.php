ee1af6543e1f870f300cc211278f4ff107e9ef84
mrajkumarken <rajkumar.m@kenhike.com>
2017-05-27 16:35:46 +0530

    url changed

--- storage/framework/views/666cf7fd1dc12e5ba415ff3d50e5f0b4d636a96a.php
+++
@@ -1,5 +0,0 @@
-<!-- Section Start -->
-<section class="title-page" style="background-color:<?php echo e($data->empty_page_color); ?>;height:100vh!important;">  
-<!-- Section To Display Empty -->
-</section> 
-<!-- Section End -->
